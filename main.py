b4dc71c3af579620b35f18a2d3e8669463d8ed75
sharath-mohan <sharathdesignsofficial@gmail.com>
2025-11-21 00:36:52 +0530

    fix: updated with lifetime

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 from fastapi import FastAPI
+from fastapi.concurrency import asynccontextmanager
 from mangum import Mangum
-app = FastAPI()
+
+import httpx
+results =[]
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    
+    global results
+    if not results:
+        async with httpx.AsyncClient() as client:
+            response = await client.get("https://dummyjson.com/test?delay=500")
+            results.append(response.json())
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": results}
 
 
 handler = Mangum(app, lifespan="auto")
+
+# def handler(event, context):
+#     global results
+
+#     response = httpx.get("https://jsonplaceholder.typicode.com/posts")
+#     results = response.json()
+          
+#     asgi_handler = Mangum(app, lifespan="off")
+#     return asgi_handler(event, context)
